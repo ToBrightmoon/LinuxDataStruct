35d42ace49cf5aea66bc7c035ed0dbf8b8c9e483
chengzhao123456 <1870035544@qq.com>
2023-02-19 15:51:22 +0800

    动态绑定与静态绑定

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 //
 // Created by cheng on 2023/2/17.
 //
-#include <iostream>
 #include "list.h"
+#include <iostream>
+class Base {
+public:
+    virtual void cout(int i = 10) const {
+      std::cout << "Base: " << i << std::endl;
+    }
+    virtual ~Base(){};
+};
+
+class Derived : public Base {
+    void cout(int i = 100) const override {
+      std::cout << "Derived: " << i << std::endl;
+    }
+};
 int main() {
+  const Base& b = Derived();
+  b.cout();
   std::cout << "hello world" << std::endl;
   return -1;
 }
