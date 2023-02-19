101f54bda63690241215b6ae568f318d8cd6367e
chengzhao123456 <1870035544@qq.com>
2023-02-19 19:33:29 +0800

    多继承

--- main.cpp
+++ main.cpp
@@ -11,14 +11,28 @@ public:
     virtual ~Base(){};
 };
 
-class Derived : public Base {
+class Base2 {
+public:
+    virtual void cy() const {
+      std::cout << "Base2" << std::endl;
+    }
+};
+class Derived : public Base , public Base2{
     void cout(int i = 100) const override {
       std::cout << "Derived: " << i << std::endl;
     }
+
+    void cy() const override {
+      std::cout << "Derived cy" << std::endl;
+    }
 };
+
+
 int main() {
   const Base& b = Derived();
+  const Base2& b2 = Derived();
   b.cout();
+  b2.cy();
   std::cout << "hello world" << std::endl;
   return -1;
 }
