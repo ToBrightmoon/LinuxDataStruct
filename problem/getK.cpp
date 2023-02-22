fd54e9047583568f39e3d8b467e8c3d62e7f0249
chengzhao123456 <1870035544@qq.com>
2023-02-22 10:45:20 +0800

    获得第K个大的元素

---
+++ problem/getK.cpp
@@ -0,0 +1,50 @@
+//
+// Created by cheng on 2023/2/22.
+//
+#include <vector>
+/*
+ * Get the k of order in a vector
+ */
+int partion(std::vector<int>& vec, int start, int end) {
+  int val = vec.at(start);
+  int i = start;
+  int j = end;
+  while (i < j) {
+    while (i < j && vec.at(j) >= val) {
+      --j;
+    }
+    if(i < j) {
+      vec.at(i) = vec.at(j);
+      ++i;
+    }
+
+    while (i <j && vec.at(i) >= val) {
+      ++i;
+    }
+
+    if(i < j) {
+      vec.at(j) = vec.at(i);
+      --j;
+    }
+  }
+  vec.at(i) = val;
+  return i;
+}
+
+int getval(std::vector<int>& vec, int start, int end, int k) {
+  int pos = partion(vec,start,end);
+  if(pos == k) {
+    return vec[k];
+  }
+  if(pos < k) {
+    return getval(vec,pos+1,end,k);
+  } else {
+    return getval(vec,start,pos-1,k);
+  }
+
+}
+int getK(std::vector<int>& vec, int k) {
+  int n = vec.size();
+  int val = getval(vec,0,n-1,k-1);
+  return val;
+}
