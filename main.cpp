9bcf3c5e1676f3ddaff46266c502a954fdd79613
chengzhao123456 <1870035544@qq.com>
2023-02-23 11:33:33 +0800

    调整了下CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -3,37 +3,12 @@
 //
 #include "list.h"
 #include <iostream>
-class Base {
-public:
-    virtual void cout(int i = 10) const {
-      std::cout << "Base: " << i << std::endl;
-    }
-    virtual ~Base(){};
-};
-
-class Base2 {
-public:
-    virtual void cy() const {
-      std::cout << "Base2" << std::endl;
-    }
-};
-class Derived : public Base , public Base2{
-    void cout(int i = 100) const override {
-      std::cout << "Derived: " << i << std::endl;
-    }
-
-    void cy() const override {
-      std::cout << "Derived cy" << std::endl;
-    }
-};
+#include <vector>
 
 
 int main() {
-  const Base& b = Derived();
-  const Base2& b2 = Derived();
-  b.cout();
-  b2.cy();
-  b2.cy();
+  std::vector<int> vec = {2};
+
   std::cout << "hello world" << std::endl;
-  return -1;
+  return 0;
 }
